9d5e799d2e5a67a8e0dc5d8c4f7fb075ae40057b
Gusthawo Junkes <gusthawojunkes14@gmail.com>
2022-04-25 21:25:07 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+title.ratings.tsv.gz – Contains the IMDb rating and votes information for titles
+
+    tconst (string) - alphanumeric unique identifier of the title
+    averageRating – weighted average of all the individual user ratings
+    numVotes - number of votes the title has received
+
+title.basics.tsv.gz - Contains the following information for titles:
+
+    tconst (string) - alphanumeric unique identifier of the title
+    titleType (string) – the type/format of the title (e.g. movie, short, tvseries, tvepisode, video, etc)
+    primaryTitle (string) – the more popular title / the title used by the filmmakers on promotional materials at the point of release
+    originalTitle (string) - original title, in the original language
+    isAdult (boolean) - 0: non-adult title; 1: adult title
+    startYear (YYYY) – represents the release year of a title. In the case of TV Series, it is the series start year
+    endYear (YYYY) – TV Series end year. ‘\N’ for all other title types
+    runtimeMinutes – primary runtime of the title, in minutes
+    genres (string array) – includes up to three genres associated with the title
---
+++ main.py
@@ -0,0 +1,58 @@
+import csv
+
+ratings_tsv = open("ratings.tsv")
+titles_tsv = open("titles.tsv")
+ratings = csv.reader(ratings_tsv, delimiter="\t")
+titles = csv.reader(titles_tsv, delimiter="\t")
+
+IMDB_DATA = dict()
+not_found = []
+
+print("Loading IMDB data...")
+for title in titles:
+    if title and len(title) > 0:
+        dictionary = {}
+        dictionary['id'] = title[0]
+        if len(title) > 1:
+            dictionary['titleType'] = title[1]
+        if len(title) > 2:
+            dictionary['primaryTitle'] = title[2]
+        if len(title) > 3:
+            dictionary['originalTitle'] = title[3]
+        if len(title) > 4:
+            dictionary['isAdult'] = title[4]
+        if len(title) > 5:
+            dictionary['startYear'] = title[5]
+        if len(title) > 6:
+            dictionary['endYear'] = title[6]
+        if len(title) > 7:
+            dictionary['runtimeMinutes'] = title[7]
+        if len(title) > 8:
+            dictionary['genres'] = title[8]
+
+        IMDB_DATA[title[0]] = dictionary
+
+
+for rating in ratings:
+    if rating and len(rating) > 0:
+        id = rating[0]
+        try:
+            if len(rating) > 1:
+                ratingValue = rating[1]
+                IMDB_DATA[id]['ratingValue'] = ratingValue
+            if len(rating) > 2:
+                numVotes = rating[2]
+                IMDB_DATA[id]['numVotes'] = numVotes
+        except KeyError:
+            not_found.append(id)
+
+
+for data in IMDB_DATA.values():
+    print(data)
+
+total = len(IMDB_DATA)
+print("Total of movies loaded: " + str(total) + "\n")
+
+with_errors = len(not_found)
+percentage_with_errors = (with_errors / total) * 100
+print("Total of movies not found: " + str(with_errors) + " (" + str(percentage_with_errors) + " of total)\n")
